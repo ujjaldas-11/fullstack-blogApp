dfd4d51379e883fd9e09623be3558cf69319dbaa
ujjaldas-11 <dasu82058@gmail.com>
2025-12-28 17:12:22 +0530

    blog page created

---
+++ blogapp/src/app/blog/[slug]/page.js
@@ -0,0 +1,34 @@
+import { supabase } from "@/lib/supabase";
+import { notFound } from "next/navigation";
+
+
+export default async function PostPage({ params }) {
+    const { slug } = await params;
+
+    const { data: post, error } = await supabase
+        .from('posts')
+        .select('*')
+        .eq('slug', slug)
+        .single(); // for exactly one post
+
+    if (error || !post) {
+        notFound(); // 404 page
+    }
+
+
+    return (
+        <article className="max-w-4xl mx-auto p-8 prose prose-lg dark:prose-invert">
+            <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+            <time className="text-gray-500 text-sm block mb-8">
+                {new Date(post.created_at).toLocaleDateString('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                })}
+            </time>
+            <div className="mt-8">
+                <p>{post.content}</p>
+            </div>
+        </article>
+    );
+}
--- blogapp/src/app/blog/page.js
+++ blogapp/src/app/blog/page.js
@@ -6,22 +6,29 @@ export default async function blogPage() {
     const {data: posts, error} = await supabase
     .from('posts')
     .select('*')
-    .order('createdAt', {ascending: false});
+    .order('created_at', {ascending: false});
 
     if(error) {
         console.log('Error fetching posts: ', error);
         return <p>Error Loading page..</p>
     }
+
+    if(!posts || posts.length == 0) {
+        return <p>No posts added yet. Please add some posts in  your supabase dashboard!</p>
+    }
+
     return(
-        <>
-            <h1>
+        <div>
+            <h1>Blog page</h1>
                 {posts.map((post)=> (
-                    <div key={post.id}>
-                        <h2>{post.title}</h2>
-                        <h2>{post.content}</h2>
+                    <div key={post.id} className='bg-white text-black border rounded-2xl w-[300px] m-10 p-4'>
+                        <a href={`/blog/${post.slug}`} className='no-underline'>
+                        <h2 className='text-center font-bold'>{post.title}</h2>
+                        </a>
+                        <h2 className='text-center'>{post.content}</h2>
+                        <p className='text-gray-500 text-sm text-right'>{new Date(post.created_at).toLocaleDateString()}</p>
                     </div>
                 ))  }
-            </h1>
-        </>
-    )
+        </div>
+    ) 
 }
